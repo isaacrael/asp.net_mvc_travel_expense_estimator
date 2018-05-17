5f7bda292ddad163ab555d867cb7abe4d5986ceb
Gil Rael <gil@gilrael.com>
2018-05-16 18:59:30 -0600

    my first commit

---
+++ Travel Expense Estimator/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Travel_Expense_Estimator.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
